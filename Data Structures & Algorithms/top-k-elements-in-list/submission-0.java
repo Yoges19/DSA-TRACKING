c6a91063c6931c8391e0f804865d4db3bba70aa3
Yogeswari B <168911924+Yoges19@users.noreply.github.com>
2026-09-17 19:21:45 +0530

    Update submission-0.java

--- Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.java
@@ -2,16 +2,20 @@ class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> map = new HashMap<>();
         //we have to return most frequent elements.Elements count should be k
-        //ex: k=2 you have return 2 element that element should be have most frequency
+        //ex: k=2 you have to return 2 element. that element should be have most frequency
 
-        // //step 1: store the element with frequency as a key, value pair in map
+        //step 1: store the element with frequency as a key, value pair in map
         for(int num : nums){
             map.put(num, map.getOrDefault(num, 0)+1);
         }
 
+        //Use MinHeap to get the most frequent value
+        // the will be store k elements if the size of pq > k we need remove smallest element so just use poll method to remove it.
+        
         PriorityQueue<Map.Entry<Integer, Integer>> pq =
                     new PriorityQueue<>(
-                        Comparator.comparingInt(Map.Entry::getValue)
+                        Comparator.comparingInt(Map.Entry::getValue) // pq compare the value here we are storing the map entry so we need to mentain which value should be compared here
+                        //Here we need compare the values inorder to get the highest frequency
                     );
 
          for(Map.Entry<Integer, Integer> entry : map.entrySet()){
@@ -22,6 +26,7 @@ class Solution {
          }
          int[] ans = new int[k];
          int i = 0;
+        // Then finally store it in array return it
          while(!pq.isEmpty()){
              Map.Entry<Integer, Integer> entry = pq.poll();
              ans[i++] = entry.getKey();
